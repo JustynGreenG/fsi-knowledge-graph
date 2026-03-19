d9047997f0d78fcdedb85eed0ae4532c0669fc7b
JustynGreenGoogle <justyngreen@google.com>
2026-03-19 19:15:49 +1100

    Delete pv-fsi-knowledge-graph directory

--- pv-fsi-knowledge-graph/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Default to app.py if not specified
-SCRIPT_NAME=${DASHBOARD_SCRIPT:-"app.py"}
-
-echo "🚀 Starting Streamlit App: $SCRIPT_NAME"
-exec streamlit run $SCRIPT_NAME --server.port $PORT --server.address 0.0.0.0
